9b8a23c326307c41af86a312a0ba668934ac5834
尹世博 <69384148+ysb19971227@users.noreply.github.com>
2022-02-08 16:52:16 +0800

    Add files via upload

---
+++ src/leetcode/editor/cn/P101SymmetricTree .java
@@ -0,0 +1,78 @@
+//给你一个二叉树的根节点 root ， 检查它是否轴对称。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,2,2,3,4,4,3]
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1,2,2,null,3,null,3]
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [1, 1000] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶：你可以运用递归和迭代两种方法解决这个问题吗？ 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1727 👎 0
+
+package leetcode.editor.cn;
+//java:对称二叉树
+public class P101SymmetricTree{
+    public static void main(String[] args){
+        Solution solution = new P101SymmetricTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetricChild(root.left,root.right);
+    }
+
+    public boolean isSymmetricChild(TreeNode p, TreeNode q) {
+        if(p == null && q != null || p != null && q == null) {
+            return false;
+        }
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSymmetricChild(p.left,q.right) && isSymmetricChild(p.right,q.left);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
